5ca269d5fc08adcbc93c660e8dd5cb34b0ad4c1f
Patrick <miraculixx@gmx.ch>
2020-08-28 09:25:25 +0200

    django 2.2 compatibility

--- tastypiex/cleanfields.py
+++ tastypiex/cleanfields.py
@@ -1,23 +1,24 @@
+"""
+"""
 
-"""
-"""
 
 class CleanBundleFieldsMixin(object):
     """
     Tastypie: remove any resource fields in a Resource's Meta
-    
+
     fields
     excludes
-    
+
     Tastypie's Resource does not process fields and excludes_fields
     as expected (that's a property of ModelResources). This mixin
     adds this capability
-    
+
     The 'resource_uri' is never excluded, unless you set Meta.exclude_uri=True
     """
+
     def alter_detail_data_to_serialize(self, request, bundle):
         data = bundle.data
-        if self._meta.fields:
+        if len(self._meta.fields or []):
             for k in [k for k in data.keys() if k not in self._meta.fields]:
                 if k != 'resource_uri':
                     del data[k]
